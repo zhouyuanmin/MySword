259975bf4f5c4d78d38d0b549d867aaab3258609
Yuanmin Zhou <1837722596@qq.com>
2020-11-04 22:22:24 +0800

    10- II. 青蛙跳台阶问题

---
+++ 10- II. 青蛙跳台阶问题.py
@@ -0,0 +1,35 @@
+"""
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+示例 1：
+输入：n = 2
+输出：2
+
+示例 2：
+输入：n = 7
+输出：21
+
+示例 3：
+输入：n = 0
+输出：1
+
+提示：
+0 <= n <= 100
+
+来源：力扣
+链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+def num_ways(self, n: int) -> int:
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    num_1 = 1
+    num_2 = 2
+    for i in range(3, n + 1):
+        num_1, num_2 = num_2, num_2 + num_1
+    return num_2 % 1000000007

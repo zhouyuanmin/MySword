3100623bef8c825c06b8019cc8f5680606e0ee64
Yuanmin Zhou <1837722596@qq.com>
2020-12-22 14:07:18 +0800

    18. 删除链表的节点

---
+++ 18. 删除链表的节点.py
@@ -0,0 +1,46 @@
+"""
+给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+返回删除后的链表的头节点。
+
+注意：此题对比原题有改动
+示例 1:
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+示例 2:
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+
+来源：力扣
+链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def delete_node(self, head: ListNode, val: int) -> ListNode:
+    # 边界处理
+    cursor = head
+    if head.val == val:
+        head = head.next
+        del cursor
+        return head
+
+    before = head
+    cursor = before.next
+    while True:
+        if cursor.val == val:
+            before.next = cursor.next
+            del cursor
+            return head
+        else:
+            before = cursor
+            cursor = before.next
